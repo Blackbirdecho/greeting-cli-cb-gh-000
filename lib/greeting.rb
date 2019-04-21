99542d08a8ff09a276751834d412f821936998ed
Khaled Ali Ahmed <s.echo.bird@gmail.com>
2019-04-21 17:40:01 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,8 @@
 # code the #greeting method here!
 def greeting(name)
+  puts "Hi! I'm HAL, what's your name?"
+  name = gets.chomp
   s = "Hello #{name}. It's nice to meet you.".strip
 puts  s
 end
-puts "Hi! I'm HAL, what's your name?"
-name = gets.chomp
+
